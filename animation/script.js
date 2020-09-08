0519c93496352997ea51a078717c5510c685e86b
Kreosot <max.derlow@gmail.com>
2020-09-08 15:45:01 +0200

    weird circle bpm thing in 'reflection'

---
+++ animation/script.js
@@ -0,0 +1,32 @@
+x=200;
+y=200;
+size=100;
+radius=30;
+
+function animate() {
+    reqAnimFrame =  window.mozRequestAnimationFrame    || //get framerate
+        window.webkitRequestAnimationFrame ||
+        window.msRequestAnimationFrame     ||
+        window.oRequestAnimationFrame
+    ;
+    reqAnimFrame(animate);
+
+
+    if(radius <= 300) {
+        radius +=3;
+    } //increase size by 1 per frame
+
+    draw();
+}
+
+function draw() {
+    let canvas  = document.getElementById("ex1");
+    let context = canvas.getContext("2d");
+
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'green';
+    context.fill();
+}
+
+animate();
