8f3fa5ae50c82b87b7e9f1573d2147a5be4a6511
Chloe <chloeismaily@gmail.com>
2020-09-15 15:29:32 +0200

    velocity changes

--- tutorial-01-/canvas.js
+++ tutorial-01-/canvas.js
@@ -5,8 +5,8 @@ let context = canvas.getContext("2d");
 
 let ballArray = {
 	"ball0": {'radius': 30, 'colour': 'red', 'velocity': 10, 'mass': 10},
-	"ball1": {'radius': 60, 'colour': 'green', 'velocity': 10, 'mass': 10},
-	"ball2": {'radius': 90, 'colour': 'cyan', 'velocity': 10, 'mass': 10},
+	"ball1": {'radius': 60, 'colour': 'green', 'velocity': 5, 'mass': 10},
+	"ball2": {'radius': 90, 'colour': 'cyan', 'velocity': 2, 'mass': 10},
 };
 
 //Fix so these are generated programatically instead.
